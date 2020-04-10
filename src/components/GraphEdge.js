1665efee4a29d55e60d26ec123da56f67e5b31d2
Abigail Chiaokhiao <abi.chi703@gmail.com>
2020-04-09 21:57:08 -0500

    Overlay & coordinates set

--- src/components/GraphEdge.js
+++ src/components/GraphEdge.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import {Text, View, ScrollView, StyleSheet} from 'react-native';
-import {MapView, Overlay, Geojson, Polyline, Marker, Callout, Polygon, Circle} from 'react-native-maps';
+import {MapView, Overlay, Geojson, Polyline, Marker, Callout, Polygon, Circle, AnimatedRegion, Animated} from 'react-native-maps';
 import styles from '../style';
 
 export default class GraphEdge extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { isLoading: true, scrollable: false };
+		region: new AnimatedRegion({latitude: 44.98295, longitude: -93.2325, latitudeDelta: 0.00415,longitudeDelta: 0.0054,});
 	}
+
+	onRegionChange(region) {
+		this.setState({ region });
+	  }
 
 	componentDidMount() {
 		return fetch(this.props.uri)
@@ -31,9 +36,8 @@ export default class GraphEdge extends Component {
 		}
 		
 		return (
-			<MapView provider={"google"} region={{latitude: 44.9747, longitude: -93.2323, latitudeDelta: 0.0061,longitudeDelta: 0.0061,}} 
-			  mapType={"hybrid"} showsPointsOfInterest={false} onMarkerPress={}>
-				
+			<MapView region={this.state.region} onRegionChange={this.onRegionChange} >
+				<Overlay image={eastBankOverlay.png} bounds={[[44.9788, -93.2379], [44.9705, -93.2271]]}/>
 			</MapView>
 		);
 	}
