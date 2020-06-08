7e44a95c887841f3385fda8fed455dbba147c8ab
Abigail Chiaokhiao <abi.chi703@gmail.com>
2020-06-07 22:43:57 -0500

    Region edits

--- src/components/GraphEdge.js
+++ src/components/GraphEdge.js
@@ -3,40 +3,60 @@ import {Text, View, ScrollView, StyleSheet} from 'react-native';
 import {MapView, Polyline} from 'react-native-maps';
 import styles from '../style';
 
+const LATITUDE = 44.98295;
+const LONGITUDE = -93.2325;
+const LATITUDE_DELTA = 0.00415;
+const LONGITUDE_DELTA = 0.0054;
 
-export default class GraphEdge extends Component {
-	constructor(routePts) {
+export default class GraphEdge extends Component 
+{
+	constructor(routePts) 
+	{
 		super(routePts);
-		this.state = { isLoading: true, scrollable: false, coordArray: [], region: new Region({latitude: 44.98295, longitude: -93.2325, latitudeDelta: 0.00415,longitudeDelta: 0.0054,})};
+		this.state = 
+		{ isLoading: true, scrollable: false, coordArray: [],
+			Region: {latitude: LATITUDE,
+			longitude: LONGITUDE,
+			latitudeDelta: LATITUDE_DELTA,
+			longitudeDelta: LONGITUDE_DELTA,
+			}
+		};
 	}
 
-	componentDidMount(){
+	componentDidMount()
+	{
 		var filename='coffman_yudof.json';//file from ./directions if origin == routePts[0] && destination == routePts[1]
 		return fetch('https://sase-labs-2020.github.io/assets/directions/'+ filename)
-			.then(response => response.json())
-			.then(responseJson => {
-				this.setState({
+		.then(response => response.json())
+		.then
+			(responseJson => {
+				this.setState
+				({
 					isLoading: false,
 					data: responseJson,
 					coordArray: this.state.data.coordinates,
 				})
 			})
-			.catch(error => console.error(error));
+		.catch(error => console.error(error));
 	}
 
-	render() {
-		if (this.state.isLoading) {
-			return (
+	render()
+	{
+		if (this.state.isLoading)
+		{
+			return 
+			(
 				<View style={{flex: 1, padding: 20}}>
 					<ActivityIndicator/>
 				</View>
 			);
 		}
 		
-		return (
-				<MapView initialRegion={this.state.region} style={styles.mapStyle}>
-					<Polyline coordinates={this.state.coordArray}/>
-				</MapView>
+		return 
+		(
+			<MapView region={this.state.Region}  style={styles.mapStyle}>
+				<Polyline coordinates={this.state.coordArray}/>
+			</MapView>
 		);
 	}
 }
